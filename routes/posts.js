1d64390d8fffbe2fb13c700ba977d067cfd4b895
Chrishani Imalsha <chrishaniimalsha@gmail.com>
2021-09-18 16:37:09 +0530

    Updated

--- routes/posts.js
+++ routes/posts.js
@@ -1,12 +1,11 @@
 const express = require('express');
-const posts = require('../models/posts');
-const Post = require('../models/posts');
+const Posts = require('../models/posts');
 
 const router = express.Router();
 
 // Save post
 router.post('/post/save', (req, res) => {
-    let newPost = new Post(req.body);
+    let newPost = new Posts(req.body);
 
     newPost.save((err) => {
         if (err) {
@@ -23,7 +22,7 @@ router.post('/post/save', (req, res) => {
 // Get post
 router.get('/post', (req, res) => {
 
-    Post.find().exec((err, posts) => {
+    Posts.find().exec((err, posts) => {
         if (err) {
             return res.status(400).json({
                 error: err
@@ -32,6 +31,39 @@ router.get('/post', (req, res) => {
         return res.status(200).json({
             success: true,
             existingPosts: posts
+        });
+    });
+});
+
+//update posts
+router.put('/post/update/:id', (req, res) => {
+    Posts.findByIdAndUpdate(
+        req.params.id,
+        {
+            $set: req.body
+        },
+        (err, posts) => {
+            if (err) {
+                return res.status(400).json({ error: err });
+            }
+            return res.status(200).json({
+                success: "Update successfuly",
+                existingPosts: posts
+            });
+        }
+    );
+});
+
+//Delete post
+router.delete('/post/delete/:id', (req, res) => {
+    Posts.findByIdAndRemove(req.params.id).exec((err, deletedPost) => {
+        if (err) {
+            return res.status(400).json({
+                message: "Delete unsuccessfully", err
+            });
+        }
+        return res.status(200).json({
+            message: "Delete successfully", deletedPost
         });
     });
 });
