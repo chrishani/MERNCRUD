acc616997224862a23c59c1fd691d365b872ce9d
Chrishani Imalsha <chrishaniimalsha@gmail.com>
2021-09-18 05:45:29 +0530

    Create server.js

---
+++ server.js
@@ -0,0 +1,21 @@
+const express = require('express');
+const mongoose = require('mongoose');
+const bodyParser = require('body-parser');
+
+const app = express();
+
+const PORT = 8000;
+const DB_URL = 'mongodb+srv://twg:twg123@mernapp.78adw.mongodb.net/mernCrud?retryWrites=true&w=majority';
+
+mongoose.connect(DB_URL, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true
+})
+    .then(() => {
+        console.log('DB connected successfuly');
+    })
+    .catch((err) => console.log('DB connection error', err));
+
+app.listen(PORT, () => {
+    console.log('App is running on ', PORT, 'Well done');
+});
